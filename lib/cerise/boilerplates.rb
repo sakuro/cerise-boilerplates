5b277149017353de28b839893ddc1e43db01bf6e
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2024-03-09 17:10:40 +0900

    $ bundle gem cerise-boilerplates

---
+++ lib/cerise/boilerplates.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "boilerplates/version"
+
+module Cerise
+  module Boilerplates
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
