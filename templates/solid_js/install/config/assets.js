eeb4fcd9b2d637bbbfcca2661179baac7eb7097d
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2024-03-15 22:50:55 +0900

    Fix spacing

--- templates/solid_js/install/config/assets.js
+++ templates/solid_js/install/config/assets.js
@@ -3,7 +3,7 @@ import { solidPlugin } from "esbuild-plugin-solid";
 
 await assets.run({
   esbuildOptionsFn: (_args, esbuildOptions) => {
-    const plugins = [...esbuildOptions.plugins, solidPlugin() ];
+    const plugins = [...esbuildOptions.plugins, solidPlugin()];
     return {
       ...esbuildOptions,
       plugins
